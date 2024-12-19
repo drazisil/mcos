d8eea4560b3684ffaec8d8b419b4f865e5a213dd
Molly Draven <drazi@duck.com>
2024-12-19 05:50:55 -0500

    refactor: remove unused files and classes from mcots package

--- packages/mcots/src/payloads/index.ts
+++
@@ -1,6 +0,0 @@
-export { ClientConnectionMessage } from "./ClientConnectMessage.js";
-export { ClientTrackingMessage } from "./ClientTrackingMessage.js";
-export { LoginMessage } from "./LoginMessage.js";
-export { LoginCompleteMessage } from "./LoginCompleteMessage.js";
-export { SetOptionsMessage } from "./SetOptionsMessage.js";
-export { StockCarInfo, StockCar } from "./StockCar.js";
