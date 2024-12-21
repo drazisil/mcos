c79d01d96a3551f35f7ade933d2ddcdda86ded8c
Molly Draven <drazi@duck.com>
2024-12-21 13:02:18 -0500

    refactor: remove unused trySocketWrite function from socketUtility

--- packages/gateway/src/socketUtility.ts
+++ packages/gateway/src/socketUtility.ts
@@ -24,28 +24,5 @@ export function tagSocketWithId({
 	connectionStamp: number;
 	id: string;
 }): TaggedSocket {
-	return { id, socket, connectionStamp};
-}
-
-/**
- * Attempts to write data to a socket and returns a promise that resolves when the write is successful,
- * or rejects if an error occurs during the write operation.
- *
- * @param socket - The tagged socket to which the data will be written.
- * @param data - The string data to be written to the socket.
- * @returns A promise that resolves when the data is successfully written, or rejects with an error if the write fails.
- */
-export async function trySocketWrite(
-	socket: TaggedSocket,
-	data: string,
-): Promise<void> {
-	return new Promise((resolve, reject) => {
-		socket.socket.write(data, (error) => {
-			if (error) {
-				reject(error);
-			} else {
-				resolve();
-			}
-		});
-	});
+	return { id, socket, connectionStamp };
 }
